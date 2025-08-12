1192c1b3b553ddf63f2c0fe3143df4877eb09de4
Md Sojib Ali <alisojib295@gmail.com>
2025-08-12 06:50:40 +0600

    Updated  navbar

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -6,17 +6,25 @@ import { DarkModeContext } from "../context/DarkModeContext";
 
 const RootLayout = () => {
   const location = useLocation();
-  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const { darkMode } = useContext(DarkModeContext);
 
   return (
-    <div className="flex min-h-screen">
+    <div
+      className={`flex min-h-screen transition-colors duration-300 ${
+        darkMode ? "bg-[#0f0f0f] text-white" : "bg-white text-black"
+      }`}
+    >
       {/* Fixed Sidebar */}
-      <div className="fixed top-0 left-0 z-50 h-full w-20 dark:bg-[#0f0f0f] transition-colors duration-300">
+      <div
+        className={`fixed top-0 left-0 z-50 h-full w-20 transition-colors duration-300 ${
+          darkMode ? "bg-[#0f0f0f]" : "bg-white"
+        }`}
+      >
         <Navbar />
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 ml-20 dark:bg-[#0f0f0f] transition-colors duration-300 overflow-y-auto">
+      <div className="flex-1 ml-20 overflow-y-auto transition-colors duration-300">
         <AnimatePresence mode="wait">
           <Outlet key={location.pathname} />
         </AnimatePresence>
